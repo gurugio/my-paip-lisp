d2de433713f58d97a8c4f02224a4a206f13d4352
Gioh Kim <gurugio@hanmail.net>
2019-11-19 22:43:29 +0100

    ch2.5

--- ch2.lisp
+++ ch2.lisp
@@ -79,9 +79,23 @@
 (defun generate-2.2 (phrase)
   "Generate a random sentence"
   (cond ((listp phrase) (mappend #'generate-2.2 phrase))
-	((rewrites phrase) (generate-2.2 (random-elt (rewrites phrase))))
-	((term-sym phrase) (list phrase))))
+        ((rewrites phrase) (generate-2.2 (random-elt (rewrites phrase))))
+        ((term-sym phrase) (list phrase))))
 
-
+;; 2.5
+(defparameter *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
 		 
 
