efe62f592bd9c930e7967042c2707f180c844bb7
Gioh Kim <gurugio@hanmail.net>
2019-11-28 21:46:13 +0100

    chap 3.3

--- ch3.lisp
+++ ch3.lisp
@@ -19,3 +19,51 @@
     len))
 
 
+(defun length1.1 (list)
+  (let ((len 0))
+    (dolist (element list len)
+      (incf len))))
+
+(defun length2 (list)
+  (let ((len 0))
+    (mapc #'(lambda (element) (incf len)) list)
+    len))
+
+;; this does not work because-of "undefined variable: len" error.
+;; (defun length3 (list)
+;;   (do ((len 0) (+ len 1)
+;;        (l list (rest l)))
+;;       ((null l) len)))
+
+;; does not work: count is an unknown keyword
+;; (defun length4 (list)
+;;   (loop for element list
+;;      count t))
+
+(defun true (x) t)
+(defun length7 (list)
+  (count-if #'true list))
+
+(defun length8 (list)
+  (if (null list)
+      0
+      (+ 1 (position-if #'true list :from-end t))))
+
+(defun length9 (list)
+  (if (null list)
+      0
+      (+ 1 (length9 (rest list)))))
+
+(defun length10 (list)
+  (length10-aux list 0))
+
+(defun length10-aux (list len-so-far)
+  (if (null list)
+      len-so-far
+      (length10-aux (rest list) (+ len-so-far 1))))
+  
+(defun length11 (list &optional (len-so-far 0))
+  (if (null list)
+      len-so-far
+      (length11 (rest list) (+ len-so-far 1))))
+
